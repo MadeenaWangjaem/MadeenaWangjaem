127aea281e8122a9a1206c612cbde0f1d83f172b
MadeenaWangjaem <41312312+MadeenaWangjaem@users.noreply.github.com>
2021-05-06 22:47:05 +0700

    'phase'

---
+++ pages/cardItem/_slug.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1>{{ item.title }}</h1>
+    <h1>{{ item.data }}</h1>
+    <nuxt-content :document="item"></nuxt-content>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({
+    isDev,
+    route,
+    store,
+    env,
+    params,
+
+    $content,
+  }) {
+    const item = await $content('cardItem', params.slug).fetch()
+    return {
+      item,
+    }
+  },
+}
+</script>
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: top;
+  text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <a-card
+      v-for="(item, index) in cardItem"
+      :key="index"
+      :title="item.title"
+      :style="{ margin: '10px', height: '100%' }"
+    >
+      {{ item.data }}
+      <nuxt-link
+        :to="{ name: 'cardItem-slug', params: { slug: item.slug } }"
+        type="primary"
+        >Detail</nuxt-link
+      >
+    </a-card>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({
+    isDev,
+    route,
+    store,
+    env,
+    params,
+    query,
+    req,
+    res,
+    redirect,
+    error,
+    $content,
+  }) {
+    const cardItem = await $content('cardItem').fetch()
+    return {
+      cardItem,
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: top;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
